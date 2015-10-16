bdbb2c3f4c2ffe5c5a073eaab6d1cfa973354833
hemildesai <desai38@purdue.edu>
2015-10-15 22:05:04 -0500

    Added fake data generator for drivers

--- database/fdata.js
+++ database/fdata.js
@@ -13,22 +13,40 @@ connection.query('USE ' + details.database);
 for (var i = 0; i < 10; i++) {
   var newUser = {};
   newUser.username = faker.internet.userName();
-  newUser.password = bcrypt.hashSync("12345", null, null)
+  newUser.password = bcrypt.hashSync("12345", null, null);
   newUser.storename = faker.company.companyName();
   newUser.storeemail = faker.internet.email();
   newUser.storephone = faker.phone.phoneNumber();
   newUser.storephone = newUser.storephone.substring(0,11).replace(/[^0-9]/g, "");
   newUser.storeaddress = faker.address.streetAddress();
+
+  var driver = {};
+  driver.username = faker.internet.userName();
+  driver.password = bcrypt.hashSync("12345", null, null);
+  driver.name = faker.name.findName();
+  driver.address = faker.address.streetAddress();
+  driver.email = faker.internet.email();
+  driver.phone = faker.phone.phoneNumber();
+  driver.phone = driver.phone.substring(0,11).replace(/[^0-9]/g, "");
+
   users.push(newUser);
-  console.log(newUser);
+  //console.log(newUser);
+
   var insertUser = "INSERT INTO users ( username, password, storename, storeemail, storephone, storeaddress) values (?,?,?,?,?,?)";
+  var insertDriver = "INSERT INTO driver (username, password, name, address, email, phone) values (?,?,?,?,?,?)";
 
   connection.query(insertUser, [newUser.username, newUser.password, newUser.storename, newUser.storeemail, newUser.storephone, newUser.storeaddress], function (err, rows) {
     if (err) {console.log(err);}
-    console.log(rows);
+    //console.log(rows);
     //newUser.id = rows.insertId;
     //return done(null, newUser);
   });
+
+  connection.query(insertDriver, [driver.username, driver.password, driver.name, driver.address, driver.email, driver.phone], function (err, rows) {
+    if(err) {throw err;}
+  })
 }
+
+
 
 connection.end();
