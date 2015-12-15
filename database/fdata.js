5dae035a95ff3cf365d54132eefd3c88fa125a17
Dinesh Gajwani <dineshgajwani@gmail.com>
2015-12-14 23:05:16 -0500

    Last Document

--- database/fdata.js
+++ database/fdata.js
@@ -55,21 +55,21 @@ for (var i = 1; i < 11; i++) {
 }
 
 
-for(var j = 0; j < 15; j++) {
-  var driver = {};
-  driver.username = faker.internet.userName();
-  driver.password = bcrypt.hashSync("12345", null, null);
-  driver.name = faker.name.findName();
-  driver.address = faker.address.streetAddress();
-  driver.email = faker.internet.email();
-  driver.phone = faker.phone.phoneNumber();
-  driver.phone = driver.phone.substring(0,11).replace(/[^0-9]/g, "");
+// for(var j = 0; j < 15; j++) {
+//   var driver = {};
+//   driver.username = faker.internet.userName();
+//   driver.password = bcrypt.hashSync("12345", null, null);
+//   driver.name = faker.name.findName();
+//   driver.address = faker.address.streetAddress();
+//   driver.email = faker.internet.email();
+//   driver.phone = faker.phone.phoneNumber();
+//   driver.phone = driver.phone.substring(0,11).replace(/[^0-9]/g, "");
 
-  var insertDriver = "INSERT INTO drivers (username, password, name, address, email, phone, id) values (?,?,?,?,?,?,?)";
+//   var insertDriver = "INSERT INTO drivers (username, password, name, address, email, phone, id) values (?,?,?,?,?,?,?)";
 
-  connection.query(insertDriver, [driver.username, driver.password, driver.name, driver.address, driver.email, driver.phone, driver.id], function (err, rows) {
-    if(err) {throw err;}
-    console.log("added driver");
-  });
-}
+//   connection.query(insertDriver, [driver.username, driver.password, driver.name, driver.address, driver.email, driver.phone, driver.id], function (err, rows) {
+//     if(err) {throw err;}
+//     console.log("added driver");
+//   });
+// }
 connection.end();
